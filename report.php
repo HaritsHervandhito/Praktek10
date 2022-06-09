a59e5498dc9f7efd390d3a85bb7a7e95ac81278a
HaritsHervandhito <99859918+HaritsHervandhito@users.noreply.github.com>
2022-06-09 22:52:18 +0700

    Add files via upload

---
+++ report.php
@@ -0,0 +1,41 @@
+<?php
+    include('koneksi2.php');
+	require'vendor/autoload.php';
+	use PhpOffice\PhpSpreadsheet\Spreadsheet;
+	use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+	header("Content-type: application/vnd-ms-excel");
+    header("Content-Disposition: attachment; filename=Report Data Siswa.xls");
+
+	$spreadsheet = new Spreadsheet();
+	$sheet = $spreadsheet->getActiveSheet();
+	$sheet->setCellValue('A1','No');
+	$sheet->setCellValue('B1','Nama');
+	$sheet->setCellValue('C1','Kelas');
+	$sheet->setCellValue('D1','Alamat');
+
+	$query = mysqli_query($koneksi2,"select * from tb_siswa");
+	$i = 2;
+	$no = 1;
+	while ($row = mysqli_fetch_array($query)) 
+	{
+		$sheet->setCellValue('A'.$i, $no);
+		$sheet->setCellValue('B'.$i, $row['nama']);
+		$sheet->setCellValue('C'.$i, $row['Kelas']);
+		$sheet->setCellValue('D'.$i, $row['alamat']);	
+		$i++;	
+	}
+
+	$styleArray = [
+				'borders' => [
+					'allBorders' => [
+						'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+					],
+				],
+			];
+	$i = $i - 1;
+	$sheet->getStyle('A1:0'.$i)->applyFromArray($styleArray);
+
+	$writer = new Xlsx($spreadsheet);
+	$writer->save('Report Data Siswa.xlsx');
+?>
